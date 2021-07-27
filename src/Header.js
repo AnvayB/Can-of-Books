6712fdc6af6e27ab28b03a6ae39dbaa99e30efd2
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-26 20:24:01 -0700

    Login files

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
+import {withAuth0} from '@auth0/auth0-react';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 
 class Header extends React.Component {
   render() {
@@ -12,9 +15,15 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        if ({this.props.auth0.isAuthenticated} {
+          <LoginButton />
+        })
+        else {
+          <LogoutButton />
+        }
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
