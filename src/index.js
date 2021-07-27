bdc2a7fd913e2ccd1564865d16cb01d652d391e6
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-27 13:58:09 -0700

    rendered properly w/o profile

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
-  <Auth0Provider domain="dev-g0x5ewvn.us.auth0.com" clientId="BjBJIr35plkJgVBU7k6klWFjX41goK94" redirectUri={window.location.origin}> 
+  <Auth0Provider domain="anvayb.us.auth0.com" clientId="fcqe9T53iKML0Dkl2y2Oa3Ea6P3lmbiJ" redirectUri="http://localhost:3000"> 
     <App />
   </Auth0Provider>,
   document.getElementById('app')
